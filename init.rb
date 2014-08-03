202d973c918b0d43d35f3100f3aaed39cae1a60b
YujiSoftware <yuji.software+github@gmail.com>
2014-08-03 18:15:43 +0900

    Fixed description

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 Redmine::Plugin.register :redmine_fixed_header do
   name 'Redmine Fixed Header plugin'
   author 'Yuji Software'
-  description 'This is a plugin for Redmine to show fixed header for issue page.'
+  description 'This is a plugin for Redmine to show fixed header in issue page.'
   version '1.0.0'
   url 'https://github.com/YujiSoftware/redmine-fixed-header'
   author_url 'http://d.hatena.ne.jp/chiheisen/'
