8e04c122842edcac213053f3d9e451d656dde1b8
YujiSoftware <yuji.software+github@gmail.com>
2018-01-09 23:58:47 +0900

    Add demo page

--- lib/redmine_fixed_header/hooks/view_issues_show_description_bottom_hook.rb
+++
@@ -1,9 +0,0 @@
-module RedmineFixedHeader
-  module Hooks
-    class ViewIssuesShowDescriptionBottomHook < Redmine::Hook::ViewListener
-      include Redmine::I18n
-      
-      render_on(:view_issues_show_description_bottom, :partial => 'issues/fixed_header', :layout => false)
-    end
-  end
-end
