d08658af2eb158e5fde7e5d0cdeab2ade78f4c5d
Paolo Battistella <paolo.battistella@webformat.com>
2022-01-22 17:58:42 +0100

    Add new flag to show fixed header according user preferences

---
+++ lib/redmine_fixed_header/patches/user_preference_model.rb
@@ -0,0 +1,21 @@
+module RedmineFixedHeader
+  module Patches
+    module UserPreferenceModelPatch
+      def self.included(base)
+        base.send(:extend, self)
+
+        base.class_eval do
+          UserPreference.safe_attributes 'enabled_fixed_header'
+        end
+      end
+
+      def enabled_fixed_header; self[:enabled_fixed_header] || '0'; end
+      def enabled_fixed_header=(value); self[:enabled_fixed_header]=value; end
+
+    end
+  end
+end
+
+unless UserPreference.included_modules.include?(RedmineFixedHeader::Patches::UserPreferenceModelPatch)
+  UserPreference.send(:include, RedmineFixedHeader::Patches::UserPreferenceModelPatch)
+end
